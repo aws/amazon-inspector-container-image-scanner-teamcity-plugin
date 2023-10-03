363f97ff86d9b80997ec306586d8060650e0414d
Will Walter <waltwilo@amazon.com>
2023-10-03 12:02:33 -0600

    Project skeleton

---
+++ amazon-inspector-plugin/amazon-inspector-plugin-common/src/main/java/com/amazon/inspector/teamcity/ScanConstants.java
@@ -0,0 +1,13 @@
+package com.amazon.inspector.teamcity;
+
+public class ScanConstants {
+	public static final String SCAN_RUN_TYPE = "amazon-inspector-build-runner";
+	public static final String MESSAGE_KEY = "messageKey";
+	public static final String ARCHIVE_PATH = "archivePath";
+	public static final String ROLE_ARN = "roleArn";
+	public static final String COUNT_CRITICAL = "countCritical";
+	public static final String COUNT_HIGH = "countHigh";
+	public static final String COUNT_MEDIUM = "countMedium";
+	public static final String COUNT_LOW = "countLow";
+	public static final String REGION = "region";
+}
