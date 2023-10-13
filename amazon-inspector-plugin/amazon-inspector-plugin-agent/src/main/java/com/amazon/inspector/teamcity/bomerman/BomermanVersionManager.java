efa1952ca2c4b9ecefc67effc716ac1d8badc0c2
Will Walter <waltwilo@amazon.com>
2023-10-13 14:51:33 -0600

    Updating config page

--- amazon-inspector-plugin/amazon-inspector-plugin-agent/src/main/java/com/amazon/inspector/teamcity/bomerman/BomermanVersionManager.java
+++ amazon-inspector-plugin/amazon-inspector-plugin-agent/src/main/java/com/amazon/inspector/teamcity/bomerman/BomermanVersionManager.java
@@ -10,8 +10,8 @@ import static com.amazon.inspector.teamcity.ScanBuildProcessAdapter.publicProgre
 public class BomermanVersionManager {
     private static final String LINUX_NAME = "linux";
     private static final String MACOS_NAME = "mac";
-    private static final String AMD64_ARCH_NAME = "x86_64";
-    private static final List<String> ARM64_ARCH_NAMES = List.of("aarch64", "amd64");
+    private static final List<String> AMD64_ARCH_NAMES = List.of("x86_64", "amd64");
+    private static final List<String> ARM64_ARCH_NAMES = List.of("aarch64");
     private static final String MACOS_ARM64_NAME = "inspector-sbomgen.macos.arm64";
     private static final String MACOS_AMD64_NAME = "inspector-sbomgen.macos.amd64";
     private static final String LINUX_ARM64_NAME = "inspector-sbomgen.linux.arm64";
@@ -23,7 +23,7 @@ public class BomermanVersionManager {
         publicProgressLogger.message(String.format("Detecting Sbom Util Version using info:\nOS: %s\nArch: %s", osName, archName));
 
         if (osName.contains(LINUX_NAME)) {
-            if (archName.contains(AMD64_ARCH_NAME)) {
+            if (AMD64_ARCH_NAMES.contains(archName)) {
                 publicProgressLogger.message("Using Linux AMD64");
                 return LINUX_AMD64_NAME;
             } else if (ARM64_ARCH_NAMES.contains(archName)) {
@@ -31,7 +31,7 @@ public class BomermanVersionManager {
                 return LINUX_ARM64_NAME;
             }
         } else if (osName.contains(MACOS_NAME)) {
-            if (archName.contains(AMD64_ARCH_NAME)) {
+            if (AMD64_ARCH_NAMES.contains(archName)) {
                 publicProgressLogger.message("Using Macos AMD64");
                 return MACOS_AMD64_NAME;
             } else if (ARM64_ARCH_NAMES.contains(archName)) {
