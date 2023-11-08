615d1a3de9819f8dc30aff1b6828cf827cbd6aab
Will Walter <waltwilo@amazon.com>
2023-11-08 15:52:31 -0700

    Sanitizing user input when the sbomgen process is created
    cr: https://code.amazon.com/reviews/CR-105785880

---
+++ amazon-inspector-plugin/amazon-inspector-plugin-agent/src/test/java/com/amazon/inspector/teamcity/sbomgen/SbomgenRunnerTest.java
@@ -0,0 +1,15 @@
+package com.amazon.inspector.teamcity.sbomgen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SbomgenRunnerTest {
+    @Test
+    public void testIsValidPath() {
+        SbomgenRunner runner = new SbomgenRunner(null, null, null, null);
+        assertTrue(runner.isValidPath("alpine:latest"));
+        assertFalse(runner.isValidPath("alpine:latest&&ls"));
+    }
+}
